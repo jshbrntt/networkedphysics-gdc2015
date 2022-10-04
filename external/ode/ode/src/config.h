c9d8374fcf79ca9490f684d3ac8126520ece652a
Joshua Barnett <joshua@barne.tt>
2022-10-04 17:49:14 +0100

    Update autotools genereated make build configurations for ode.

--- external/ode/ode/src/config.h
+++ external/ode/ode/src/config.h
@@ -121,7 +121,7 @@
 #define HAVE_STDARG_H 1
 
 /* Define to 1 if stdbool.h conforms to C99. */
-#define HAVE_STDBOOL_H 1
+/* #undef HAVE_STDBOOL_H */
 
 /* Define to 1 if you have the <stddef.h> header file. */
 #define HAVE_STDDEF_H 1
