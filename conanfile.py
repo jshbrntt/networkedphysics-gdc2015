648cf7a1387ee0194a3c515d3eafad5ed64290ad
Joshua Barnett <joshua@barne.tt>
2025-10-16 21:14:14 +0000

    add joly physics

---
+++ conanfile.py
@@ -0,0 +1,19 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+
+class NetworkedPhysicsRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires("joltphysics/5.2.0")
+
+    def configure(self):
+        # Configure JoltPhysics options
+        self.options["joltphysics"].shared = False
+        self.options["joltphysics"].double_precision = False
+        self.options["joltphysics"].cross_platform_deterministic = False
+        self.options["joltphysics"].enable_asserts = True
+
+    def layout(self):
+        cmake_layout(self)
